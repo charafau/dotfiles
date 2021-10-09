7658597b71159a4b11166f6d323324d4cefe1773
Rafal Wachol <gorudonu@gmail.com>
2021-10-10 00:48:44 +0100

    Add files via upload

---
+++ download_flutter_engine.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+if [ $# -eq 0 ]
+then 
+	echo "Downloading engine with hash from default directory"
+	hash=`cat $HOME/Utils/flutter/bin/internal/engine.version`
+	echo "Read hash $hash"
+	wget https://storage.googleapis.com/flutter_infra_release/flutter/$hash/linux-x64/linux-x64-embedder
+
+else
+	echo "Downloading engine with hash $1..."
+	wget https://storage.googleapis.com/flutter_infra_release/flutter/$1/linux-x64/linux-x64-embedder
+fi
