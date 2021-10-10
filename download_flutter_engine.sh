c0cec68fb30a4fa7103d0c22d5b4e1521d0388e0
Rafal Wachol <gorudonu@gmail.com>
2021-10-10 01:03:03 +0100

    Update download_flutter_engine.sh

--- download_flutter_engine.sh
+++ download_flutter_engine.sh
@@ -6,8 +6,10 @@ then
 	hash=`cat $HOME/Utils/flutter/bin/internal/engine.version`
 	echo "Read hash $hash"
 	wget https://storage.googleapis.com/flutter_infra_release/flutter/$hash/linux-x64/linux-x64-embedder
+	wget https://storage.googleapis.com/flutter_infra_release/flutter/$hash/linux-x64/artifacts.zip
 
 else
 	echo "Downloading engine with hash $1..."
 	wget https://storage.googleapis.com/flutter_infra_release/flutter/$1/linux-x64/linux-x64-embedder
+	wget https://storage.googleapis.com/flutter_infra_release/flutter/$1/linux-x64/artifacts.zip
 fi
